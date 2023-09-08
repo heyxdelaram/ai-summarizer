e3b03997c392e1bb00a9f922603b80ba0e178aea
Delaram Janfarsa <dellafarsa@gmail.com>
2023-09-08 00:21:25 -0700

    show summary implemented

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,8 +9,16 @@ const Demo = () => {
     summary: "",
   });
 
+  const [allTexts, setAllTexts] = useState([]);
   //since the hook is lazy, the first param is a function for getting the content and the secon is an object which we can get the error and the state of the request by destructuring it
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  //dependency array gets executed at the start of the application
+  useEffect(() => {
+    const textFromLocalStorage = JSON.parse(localStorage.getItem("texts"));
+
+    if (textFromLocalStorage) setAllTexts(textFromLocalStorage);
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,7 +27,10 @@ const Demo = () => {
 
     if (data?.summary) {
       const newText = { ...text, summary: data.summary };
+      const newAllText = [...allTexts, newText];
       setText(newText);
+      setAllTexts(newAllText);
+      localStorage.setItem("texts", JSON.stringify(newAllText));
       console.log(newText);
     }
   };
@@ -55,8 +66,56 @@ const Demo = () => {
           </button>
         </form>
         {/*browse url history*/}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allTexts.map((text, i) => (
+            <div
+              key={`link-${i}`}
+              className="link_card"
+              onClick={() => setText(text)}
+            >
+              <div className="copy_btn">
+                <img
+                  src="copy"
+                  className="w-[40%] h-[40%] object-contain"
+                  alt="copy-icon"
+                />
+                <p className="flex-1 font-satoshi text-blue-700 text-sm truncate">
+                  {item.url}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       {/*Display results*/}
+
+      <div
+        className="my-10
+      max-w-full flex justify-center items-center"
+      >
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          text.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Text <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p>{text.summary}</p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
